ab8e27efc9448c3a81cca8dd38afd230dd67e981
taylor <376525082@qq.com>
2020-09-27 23:06:42 +0800

    无重复字符的最长子串

---
+++ src/q3_无重复字符的最长子串/Solution.java
@@ -0,0 +1,33 @@
+package q3_无重复字符的最长子串;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 滑动窗口
+ */
+public class Solution {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
+
