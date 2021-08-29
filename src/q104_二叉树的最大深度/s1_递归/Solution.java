19b651c8e5700e10f25bb3c72a29c2eaad1e8406
taylor <376525082@qq.com>
2021-08-29 21:48:43 +0800

    二叉树的最大深度

---
+++ src/q104_二叉树的最大深度/s1_递归/Solution.java
@@ -0,0 +1,14 @@
+package q104_二叉树的最大深度.s1_递归;
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
---
+++ src/q104_二叉树的最大深度/s1_递归/TreeNode.java
@@ -0,0 +1,20 @@
+package q104_二叉树的最大深度.s1_递归;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
