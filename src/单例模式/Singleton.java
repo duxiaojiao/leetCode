4ab3793100a6db56cb64870d16774838d934e0ce
taylor <376525082@qq.com>
2022-02-09 19:34:15 +0800

    单例模式

---
+++ src/单例模式/Singleton.java
@@ -0,0 +1,20 @@
+package 单例模式;
+
+public class Singleton {
+
+    private static volatile Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
