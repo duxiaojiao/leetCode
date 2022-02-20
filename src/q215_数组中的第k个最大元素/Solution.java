c4637d4e80d55c754788689ef6c43f9232c81621
taylor <376525082@qq.com>
2022-02-20 10:15:19 +0800

    数组中的第k个最大元素

---
+++ src/q215_数组中的第k个最大元素/Solution.java
@@ -0,0 +1,17 @@
+package q215_数组中的第k个最大元素;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) {
+            queue.offer(num);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return queue.poll();
+    }
+}
